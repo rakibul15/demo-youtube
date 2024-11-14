729b393b49d281f9254729aa1fe38a1e8079e668
rakibul.hasan <rakibul.hasan@technonext.com>
2024-11-14 11:14:51 +0600

    Update MiddleWare

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       return next(new ApiError(401, "Unauthorized Request"));
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decodedToken?._id).select("-password -refreshToken -__v -_id");
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken -__v");
     if (!user) {
       return next(new ApiError(401, "Invalid access token"));
     }
