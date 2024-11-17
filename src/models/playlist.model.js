581ef06a85d9a92a3a7c775ee7dbaf811bd1936a
rakibul.hasan <rakibul.hasan@technonext.com>
2024-11-17 16:20:04 +0600

    Add comment, like, playlist, tweet model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const playListSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    require:true
+  },
+  video: [{
+    type: Schema.Types.ObjectId,
+    ref: "Video",
+  }],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  }
+},{timestamps:true})
+
+export const PlayList = mongoose.model("PlayList", playListSchema);
