83c70b28d5c399ee7cabbf5b9dea6f6848053116
rakibul.hasan <rakibul.hasan@technonext.com>
2024-11-18 15:17:41 +0600

    edit validation of User controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -50,7 +50,11 @@ const registerUser = asyncHandler(async (req, res, next) => {
   if (req.files && Array.isArray(req.files?.coverImage) && req.files?.coverImage?.length > 0) {
     coverImageLocalPath = req.files?.coverImage[0]?.path;
   }
-  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  let avatarLocalPath;
+  if (req.files && Array.isArray(req.files?.avatar) && req.files?.avatar?.length > 0) {
+    avatarLocalPath = req.files?.avatar[0]?.path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
